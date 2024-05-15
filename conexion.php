326fa167ceda6548292d09738a62f998b5c8c71b
Aldo Tolentino <tolentinodomingodiego@gmail.com>
2024-05-14 21:09:10 -0600

    Cambios de style

--- conexion.php
+++ conexion.php
@@ -1,11 +1,17 @@
 <?php
     //CREDENCIALES PARA HACER LA CONEXION A LA BASE DE DATOS
-    $DATABASE_HOST='localhost';
-    $DATABASE_USER='root';
-    $DATABASE_PASS='';
-    $DATABASE_NAME='db_registros';
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'db_registros';
 
-    //CONEXION A LA BASE DE DATOS...
+    // Intentar realizar la conexión a la base de datos
     $conex = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
+    // Verificar si se estableció la conexión exitosamente
+    if (mysqli_connect_errno()) {
+        // En caso de error, imprimir mensaje de error y terminar el script
+        echo "Error al conectar a la base de datos MySQL: " . mysqli_connect_error();
+        exit();
+    }
 ?>
