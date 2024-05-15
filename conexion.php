758c2fd337558ae50d86edc31e59307366a7e542
Aldo Tolentino <tolentinodomingodiego@gmail.com>
2024-05-14 22:29:52 -0600

    ..

--- conexion.php
+++ conexion.php
@@ -1,17 +1,11 @@
 <?php
     //CREDENCIALES PARA HACER LA CONEXION A LA BASE DE DATOS
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'db_registros';
+    $DATABASE_HOST='localhost';
+    $DATABASE_USER='root';
+    $DATABASE_PASS='';
+    $DATABASE_NAME='db_registros';
 
-    // Intentar realizar la conexión a la base de datos
+    //CONEXION A LA BASE DE DATOS...
     $conex = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
-    // Verificar si se estableció la conexión exitosamente
-    if (mysqli_connect_errno()) {
-        // En caso de error, imprimir mensaje de error y terminar el script
-        echo "Error al conectar a la base de datos MySQL: " . mysqli_connect_error();
-        exit();
-    }
 ?>
