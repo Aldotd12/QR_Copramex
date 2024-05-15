b3982e17f2c13e7f7fa89fa62e1d124bc6b01be8
Aldo Tolentino <tolentinodomingodiego@gmail.com>
2024-05-15 10:51:11 -0600

    subir DB

--- README.md
+++
@@ -1,2 +0,0 @@
-# QR_Copramex
-[![Netlify Status](https://api.netlify.com/api/v1/badges/9c7e81c2-fd29-4dcf-919c-f4e2e186aa6c/deploy-status)](https://app.netlify.com/sites/registrociudadano/deploys)
--- conexion.php
+++ conexion.php
@@ -1,9 +1,10 @@
 <?php
     //CREDENCIALES PARA HACER LA CONEXION A LA BASE DE DATOS
-    $DATABASE_HOST='localhost';
+    $DATABASE_HOST='viaduct.proxy.rlwy.net';
     $DATABASE_USER='root';
-    $DATABASE_PASS='';
-    $DATABASE_NAME='db_registros';
+    $DATABASE_PASS='CRmYPGUcWLZlFUTWwBNGNQwurtakBNdJ';
+    $DATABASE_NAME='railway';
+    $DATABASE_PORT='49922';
 
     //CONEXION A LA BASE DE DATOS...
     $conex = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
