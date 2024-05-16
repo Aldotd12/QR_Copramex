8b9a88650a84c9078188f6277f237f2e917a52e0
Aldo Tolentino <tolentinodomingodiego@gmail.com>
2024-05-15 23:51:54 -0600

    revisando cambios

--- conexion.php
+++ conexion.php
@@ -1,12 +1,10 @@
 <?php
     //CREDENCIALES PARA HACER LA CONEXION A LA BASE DE DATOS
-    $DATABASE_HOST='viaduct.proxy.rlwy.net';
+    $DATABASE_HOST='localhost';
     $DATABASE_USER='root';
-    $DATABASE_PASS='CRmYPGUcWLZlFUTWwBNGNQwurtakBNdJ';
-    $DATABASE_NAME='railway';
-    $DATABASE_PORT='49922';
+    $DATABASE_PASS='';
+    $DATABASE_NAME='db_registros';
 
     //CONEXION A LA BASE DE DATOS...
     $conex = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-
 ?>
