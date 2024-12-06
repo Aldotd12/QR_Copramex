8f225d8d4c28c09ef1c2f7b68898dc87e0c7fd2d
Aldo Tolentino Domingo <147024614+Aldotd12@users.noreply.github.com>
2024-12-05 21:35:34 -0600

    Update conexion.php
    Se hizo cambio de los datos

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
     //CREDENCIALES PARA HACER LA CONEXION A LA BASE DE DATOS
-    $DATABASE_HOST='localhost';
-    $DATABASE_USER='root';
+    $DATABASE_HOST='';
+    $DATABASE_USER='';
     $DATABASE_PASS='';
-    $DATABASE_NAME='db_registros';
+    $DATABASE_NAME='';
 
     //CONEXION A LA BASE DE DATOS...
     $conex = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
